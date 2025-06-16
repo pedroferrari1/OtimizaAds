31cb34d868a599b93b70ce43b194fb7cf98d9344
marco-2806 <marcoaurelio2806@gmail.com>
2025-06-16 07:17:08 -0300

    Corrigir importações duplicadas no SubscriptionDetails

--- src/features/subscription/components/SubscriptionDetails.tsx
+++ src/features/subscription/components/SubscriptionDetails.tsx
@@ -7,8 +7,6 @@ import { Calendar, RefreshCw, Settings, CreditCard, AlertTriangle, CheckCircle }
 import { UserSubscription } from "@/types/subscription";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
 
 interface SubscriptionDetailsProps {
   userSubscription: UserSubscription;
